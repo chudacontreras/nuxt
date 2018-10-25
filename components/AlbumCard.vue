d91826374854b98a3a8a68005d3a6b8514c17406
chudacontreras <chudacontreras@gmail.com>
2018-10-25 14:28:38 -0400

    cambios varios, agregado el css bulma

---
+++ components/AlbumCard.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <div class="column is-one-quarter">
+        <div class="box">
+        <h3>{{album.title}}</h3>
+        <a href="#" class="button is-primary">Ver Álbum</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "AlbumCard",
+  props: {
+    album: Object
+  }
+};
+</script>
+
+
+<style lang="css">
+.box{
+    paddiing:1em;
+}
+h3{
+    min-height: 5em;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,29 @@
 <template lang="html">
-
-<h1>...{{mensaje}}...</h1>
-
-    
+<div class="container">
+<h1 class="title">...::...Albums de la pagina ...::...</h1>
+<div class="columns is-multiline">
+<AlbumCard :album="album" v-for="album in albums" :key="album.id" />
+</div> 
+</div> 
 </template>
 
 <script>
 import axios from 'axios';
 import env from '../config/env';
+import AlbumCard from '../components/AlbumCard';
 export default {
     name: 'IndexPage',
+    components :{
+        AlbumCard
+    },
     data(){
         return{
-            mensaje: ''
+            albums: []
         }
     },
     created(){
         axios.get(`${env.endpoint}/albums`).then(response=>{
-            this.mensaje = response.status;
+            this.albums = response.data;
         })
     }
 }
